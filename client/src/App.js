8d008bcbe9bd7dc628faae5e26fd71f321b4e289
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-01-24 00:30:28 +0100

    add AuthContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import {AuthContext} from './context/AuthContex'
 import useRoutes from './features/UseRoutes'
+import {useAuth} from './hooks/auth.hook'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import bootstrap from 'bootstrap'
 
 function App() {
-    const routes = useRoutes(true)
+    const {token, logIn, logOut, userId} = useAuth()
+    const isAuthenticated = !!token
+    const routes = useRoutes(isAuthenticated)
     return (
+        <AuthContext.Provider value={{token, logIn, logOut, userId, isAuthenticated}}>
             {routes}
+        </AuthContext.Provider>
     )
 }
 
