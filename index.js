5a56b6ac97802e716822a1eb2bb2ead233778670
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-01-31 19:38:28 +0100

    add express middleware wich add user model in every requests

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const User = require('./models/user')
 const PORT = '5000'
 const MongoURI =
     'mongodb+srv://MaksimenkoYr:1337@time-tracker.xkvyz.mongodb.net/time-tracker?retryWrites=true&w=majority'
 
 const app = express()
 app.use(express.json({extendet: true}))
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById(req.headers['user-id'])
+        req.user = user
+    } catch (e) {
+        console.log(e)
+    }
+    next()
+})
+app.use('/activity', require('./routes/activity.router'))
 app.use('/api/auth', require('./routes/auth.router'))
+
 async function start() {
     try {
         await mongoose.connect(MongoURI, {
@@ -20,7 +32,7 @@ async function start() {
             console.log(`server starterd at port: ${PORT}`)
         })
     } catch (error) {
-        console.log('Server Error', e.message)
+        console.log('Server Error', error.message)
         process.exit(1)
     }
 }
