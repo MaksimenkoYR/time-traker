aeab04da11ab8a996ea3b0a3022ac8bdf303e1f5
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-02-09 15:27:33 +0100

    add activity.router and update user model

--- models/user.js
+++ models/user.js
@@ -7,9 +7,8 @@ const user = new Schema({
     activity: [
         {
             name: {type: String, required: true},
-            date: {type: Date, required: true},
             start: {type: Date, required: true},
-            end: {type: Date, required: true},
+            end: {type: Date},
         },
     ],
 })
@@ -19,4 +18,23 @@ user.methods.addActivityTypes = function (newActivityTypes) {
     return this.save()
 }
 
+user.methods.addActivity = function (newActivity) {
+    this.activity = [...this.activity, {name: newActivity.name, start: newActivity.start}]
+    return this.save()
+}
+user.methods.updateActivity = function (activity) {
+    console.log('activity', activity)
+    this.activity = this.activity.map(item => {
+        if (item.id === activity.id) {
+            return {
+                name: activity.name || item.name,
+                start: activity.start || item.start,
+                end: activity.end || item.end,
+            }
+        } else {
+            return item
+        }
+    })
+    return this.save()
+}
 module.exports = model('User', user)
---
+++ routes/activity.router.js
@@ -0,0 +1,47 @@
+const {Router} = require('express')
+
+router = Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const startedActivity = req.user.activity.filter(item => item.end == null)
+        console.log(startedActivity)
+        res.status(200).send({
+            activityTypes: req.user.activityTypes,
+            startedActivity,
+        })
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.post('/types', async (req, res) => {
+    try {
+        const newActivityTypes = req.body.activityName
+        req.user.addActivityTypes(newActivityTypes)
+        res.status(201).send({message: 'activity type created'})
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.post('/start', async (req, res) => {
+    try {
+        const newActivity = {name: req.body.activityType, start: req.body.start}
+        req.user.addActivity(newActivity)
+        res.status(201).send({message: 'activity started'})
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.post('/:id/end', async (req, res) => {
+    try {
+        req.user.updateActivity(req.body)
+        res.status(201).send({message: 'acntivity end'})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
